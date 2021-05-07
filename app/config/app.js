623a71339635e8769b6515cbb133d19c3109209c
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-05-08 01:20:41 +0800

    initial configuration

---
+++ app/config/app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    rootPath: path.resolve(__dirname, '../../'),
+    serviceName: process.env.SERVICE_NAME,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbPort: process.env.DB_PORT,
+    dbName: process.env.DB_NAME,
+}
---
+++ database/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { dbHost, dbUser, dbPassword, dbPort, dbName } = require('../app/config/app');
+
+mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?authSource=admin`, {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+module.exports = mongoose.connection;
