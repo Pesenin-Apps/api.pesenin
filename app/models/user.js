6b57c71fcdbbe72c926116c9ac719fc049f85e39
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-05-08 02:37:29 +0800

    add user schema

---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { model, Schema } = mongoose;
+
+const userSchema = Schema({
+    fullname: {
+        type: String,
+        required: [ true, 'Nama harus diisi' ],
+        maxlength: [ 255, 'Panjang nama harus antara 3 - 255 karakter' ],
+        minlength: [ 3, 'Panjang nama harus antara 3 - 255 karakter' ]
+    },
+    email: {
+        type: String,
+        required: [ true, 'Email harus diisi' ],
+        maxlength: [255, 'Panjang email maksimal 255 karakter'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password harus diisi'],
+        maxlength: [255, 'Panjang password maksimal 255 karakter'],
+    },
+    role_id: {
+        type: Number
+    },
+    token: [String]
+}, { timestamps: true });
+
+module.exports = model('User', userSchema);
