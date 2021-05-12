aa2e066a53f75e590bebf9993ad7e2b71ca7018c
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-05-12 15:37:40 +0800

    add all actors on policy

--- app/policy/index.js
+++ app/policy/index.js
@@ -2,4 +2,24 @@ const { AbilityBuilder, Ability } = require("@casl/ability");
 
 const policies = {
     
+    guest(user, { can }){
+        
+    },
+
+    customer(user, { can }){
+
+    },
+
+    waiter(user, { can }){
+
+    },
+
+    chef(user, { can }){
+        
+    },
+
+    cashier(user, { can }){
+
+    }
+
 }
