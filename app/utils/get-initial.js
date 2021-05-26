7678314d1a3a2d7edcb2a1bd645731cd2649d16e
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-05-26 22:55:02 +0800

    fix customer controller and add method getNumbering

--- app/utils/get-initial.js
+++
@@ -1,8 +0,0 @@
-function getInitial(str) {
-    let initial = str.split(/\s/).reduce((response,word)=> response+=word.slice(0,1),'');
-    return initial;
-}
-
-module.exports = {
-    getInitial
-}
