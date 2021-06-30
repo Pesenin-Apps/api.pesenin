e7d7ee384f92e8c1f3310216cc652a94104012e4
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-06-30 10:49:46 +0800

    add method getNumbering in helpers/gets

--- app/helpers/gets.js
+++ app/helpers/gets.js
@@ -3,3 +3,20 @@ function getInitial(str) {
     const initial = str.split(/\s/).reduce((response,word) => response += word.slice(0,1), '');
     return initial;
 }
+
+// get numbering unique
+function getNumbering(options) {
+    let strOption;
+    let dateNow = Date.now();
+    switch (options) {
+        case 'checkin':
+            strOption = 'CHECKIN';
+            break;
+        case 'order':
+            strOption = 'ORDER';
+            break;
+        default:
+            strOption = 'NUMBER';
+    }
+    return strOption + '#' + dateNow;
+}
