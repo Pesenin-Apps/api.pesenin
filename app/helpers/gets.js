5c0e8568e72f2a4bfac9d0136b96f6990c16ef1a
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-06-30 12:09:33 +0800

    add method getCustomerCheckedIn in helpers/gets

--- app/helpers/gets.js
+++ app/helpers/gets.js
@@ -1,3 +1,5 @@
+const { Customer } = require('../models/customer')
+
 // get initial based on `params`, ex: params = `Tiyan Attirmdzi` then return `TA`
 function getInitial(str) {
     const initial = str.split(/\s/).reduce((response,word) => response += word.slice(0,1), '');
@@ -19,4 +21,10 @@ function getNumbering(options) {
             strOption = 'NUMBER';
     }
     return strOption + '#' + dateNow;
+}
+
+// get customer who checked in
+async function getCustomerCheckedIn(checkInNumber) {
+    const customer = await Customer.findOne({ checkin_number: {$in: checkInNumber} });
+    return customer;
 }
