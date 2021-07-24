eb8de6214568dabcb4347f47871e04719c6792a0
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-07-25 07:30:18 +0800

    refactor middleware hasRole to hasStaff

--- routes/app.js
+++ routes/app.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
-const { hasRole, hasCustomer } = require('../app/middlewares/authentication');
+const { hasStaff, hasCustomer } = require('../app/middlewares/authentication');
 
 const customerController = require('../app/controllers/customer.controller');
 const authController = require('../app/controllers/auth.controller');
@@ -50,13 +50,13 @@ router.get('/user/me', staffController.me);
 
 /* ========= START ENDPOINT FOR WAITER ========= */
 
-router.get('/waiters/orders/customers', hasRole('waiter'), orderController.getCustomerOrdersForWaiters);
-router.get('/waiters/orders/all', hasRole('waiter'), ordersController.getAllOrders);
-router.get('/waiters/orders', hasRole('waiter'), ordersController.getOrderForWaiter);
-router.post('/waiters/orders/customers', hasRole('waiter'), orderController.storeForWaiter);
-router.post('/waiters/change-status', hasRole('waiter'), staffController.changeStatus);
-router.post('/waiters/orders/verify/:id', hasRole('waiter'), orderController.verifyCustomerOrders);
-router.patch('/waiters/orders/:id', hasRole('waiter'), orderController.updateForWaiter);
+router.get('/waiters/orders/customers', hasStaff('waiter'), orderController.getCustomerOrdersForWaiters);
+router.get('/waiters/orders/all', hasStaff('waiter'), ordersController.getAllOrders);
+router.get('/waiters/orders', hasStaff('waiter'), ordersController.getOrderForWaiter);
+router.post('/waiters/orders/customers', hasStaff('waiter'), orderController.storeForWaiter);
+router.post('/waiters/change-status', hasStaff('waiter'), staffController.changeStatus);
+router.post('/waiters/orders/verify/:id', hasStaff('waiter'), orderController.verifyCustomerOrders);
+router.patch('/waiters/orders/:id', hasStaff('waiter'), orderController.updateForWaiter);
 
 /* ========= END ENDPOINT FOR WAITER ========= */
 
@@ -66,23 +66,23 @@ router.patch('/waiters/orders/:id', hasRole('waiter'), orderController.updateFor
 // type
 router.get('/products/types', productTypeController.index);
 router.get('/products/types/:id', productTypeController.show);
-router.post('/products/types', [ hasRole('cashier'), multer().none() ], productTypeController.store);
-router.patch('/products/types/:id', [ hasRole('cashier'), multer().none() ], productTypeController.update);
-router.delete('/products/types/:id', hasRole('cashier'), productTypeController.destroy);
+router.post('/products/types', [ hasStaff('cashier'), multer().none() ], productTypeController.store);
+router.patch('/products/types/:id', [ hasStaff('cashier'), multer().none() ], productTypeController.update);
+router.delete('/products/types/:id', hasStaff('cashier'), productTypeController.destroy);
 
 // category
 router.get('/products/categories', productCategoryController.index);
 router.get('/products/categories/:id', productCategoryController.show);
-router.post('/products/categories', [ hasRole('cashier'), multer().none() ], productCategoryController.store);
-router.patch('/products/categories/:id', [ hasRole('cashier'), multer().none() ], productCategoryController.update);
+router.post('/products/categories', [ hasStaff('cashier'), multer().none() ], productCategoryController.store);
+router.patch('/products/categories/:id', [ hasStaff('cashier'), multer().none() ], productCategoryController.update);
 router.delete('/products/categories/:id', productCategoryController.destroy);
 
 // product
 router.get('/products', productController.index);
 router.get('/products/:id', productController.show);
-router.post('/products', [ hasRole('cashier'), multer({dest: os.tmpdir()}).single('image') ], productController.store);
-router.patch('/products/:id', [ hasRole('cashier'), multer({dest: os.tmpdir()}).single('image') ], productController.update);
-router.delete('/products/:id', hasRole('cashier'), productController.destroy);
+router.post('/products', [ hasStaff('cashier'), multer({dest: os.tmpdir()}).single('image') ], productController.store);
+router.patch('/products/:id', [ hasStaff('cashier'), multer({dest: os.tmpdir()}).single('image') ], productController.update);
+router.delete('/products/:id', hasStaff('cashier'), productController.destroy);
 
 /* ========= END PRODUCT ENDPOINT ========= */
 
@@ -92,16 +92,16 @@ router.delete('/products/:id', hasRole('cashier'), productController.destroy);
 // section
 router.get('/tables/sections', tableSectionController.index);
 router.get('/tables/sections/:id', tableSectionController.show);
-router.post('/tables/sections', [ hasRole('cashier'), multer().none() ], tableSectionController.store);
-router.patch('/tables/sections/:id', [ hasRole('cashier'), multer().none() ], tableSectionController.update);
-router.delete('/tables/sections/:id', hasRole('cashier'), tableSectionController.destroy);
+router.post('/tables/sections', [ hasStaff('cashier'), multer().none() ], tableSectionController.store);
+router.patch('/tables/sections/:id', [ hasStaff('cashier'), multer().none() ], tableSectionController.update);
+router.delete('/tables/sections/:id', hasStaff('cashier'), tableSectionController.destroy);
 
 // table
 router.get('/tables', tableController.index);
 router.get('/tables/:id', tableController.show);
-router.post('/tables', [ hasRole('cashier'), multer().none() ], tableController.store);
-router.patch('/tables/:id', [ hasRole('cashier'), multer().none() ], tableController.update);
-router.delete('/tables/:id', hasRole('cashier'), tableController.destroy);
+router.post('/tables', [ hasStaff('cashier'), multer().none() ], tableController.store);
+router.patch('/tables/:id', [ hasStaff('cashier'), multer().none() ], tableController.update);
+router.delete('/tables/:id', hasStaff('cashier'), tableController.destroy);
 
 /* ========= END TABLE ENDPOINT ========= */
 
