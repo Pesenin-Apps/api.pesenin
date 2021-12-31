3257d010f6a4eee7ac6a2346a594d4b3d050bc00
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2021-12-31 12:38:10 +0800

    add createReservationByCustomer

---
+++ app/models/orders/reservation.js
@@ -0,0 +1,55 @@
+const { model, Schema } = require('mongoose');
+
+const STATUS = {
+    CREATE: 1,
+    CONFIRMED: 2,
+}
+
+const SERVING_TYPE = {
+    ON_TIME: 1,
+    BY_CONFIRMATION: 2,
+}
+
+const RESERVATION_CONFIRM = {
+    WAITING: 1,
+    START_PROCESS: 2,
+}
+
+const reservationSchema = Schema({
+    order: {
+        type: Schema.Types.ObjectId,
+        ref: 'Order',
+    }, 
+    datetime_plan: {
+        type: Date,
+        required: [ true, 'is required.' ],
+    },
+    number_of_people: {
+        type: Number,
+        required: [ true, 'is required.' ],
+    },
+    section_table: {
+        type: Schema.Types.ObjectId,
+        ref: 'TableSection',
+        required: [ true, 'is required.' ]
+    },
+    status: {
+        type: Number,
+        default: STATUS.CREATE,
+    },
+    serving_type: {
+        type: Number,
+        required: [ true, 'is required.' ],
+    },
+    reservartion_confirm: {
+        type: Number,
+        default: null,
+    },
+});
+
+module.exports = {
+    STATUS_RESERVATION: STATUS,
+    SERVING_TYPE,
+    RESERVATION_CONFIRM,
+    Reservation: model('Reservation', reservationSchema)
+}
